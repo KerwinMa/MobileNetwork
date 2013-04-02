7bbf9d8595be155c46ea5744340d3d745751c76a
ljhokgo <ljhokgo@gmail.com>
2013-04-02 17:07:13 +0900

    hi

---
+++ Project1/src/Client.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Client
+{
+	static String ip = "127.0.0.1";
+	static int server_port = 1234;
+
+	public static void main(String[] args) throws Exception
+	{
+		// TODO Auto-generated method stub
+		DatagramSocket clientSocket = new DatagramSocket();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true)
+		{
+			String sentence = br.readLine();
+			byte[] buf = sentence.getBytes();
+			InetAddress IPAddress = InetAddress.getByName(ip);
+			DatagramPacket sendPacket = new DatagramPacket(buf, buf.length,
+					IPAddress, server_port);
+			clientSocket.send(sendPacket);
+			byte[] recv_buf = new byte[1024];
+			DatagramPacket recvPacket = new DatagramPacket(recv_buf,
+					recv_buf.length);
+			clientSocket.receive(recvPacket);
+			System.out.print(new String(recvPacket.getData(), 0, recvPacket.getLength()));
+			System.out.println("  from" + recvPacket.getAddress() + " port:" + recvPacket.getPort());
+		}
+	}
+}
---
+++ Project1/src/Server.java
@@ -0,0 +1,32 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Server
+{
+	static int server_port = 1234;
+
+	public static void main(String[] args) throws Exception
+	{
+		// TODO Auto-generated method stub
+		DatagramSocket serverSocket = new DatagramSocket(server_port);
+		byte[] buf;
+		byte[] sendData;
+		while (true)
+		{
+			buf = new byte[1024];
+			DatagramPacket receivePacket = new DatagramPacket(buf, buf.length);
+			serverSocket.receive(receivePacket);
+			String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+			InetAddress IPAddress = receivePacket.getAddress();
+			int port = receivePacket.getPort();
+			String capitalizedSentence = sentence.toUpperCase();
+			System.out.println(capitalizedSentence);
+			System.out.println("  from" + receivePacket.getAddress() + " port:" + receivePacket.getPort());
+			sendData = capitalizedSentence.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData,
+					sendData.length, IPAddress, port);
+			serverSocket.send(sendPacket);
+		}
+	}
+}
